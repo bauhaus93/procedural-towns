759cd4037abc7a1c2fd11cbe33d73b68f0068e68
Jakob <jakobfischer93@gmail.com>
2019-04-19 21:31:13 +0200

    Moved some resourced, reflected in script changes.

--- resources/build.py
+++ resources/build.py
@@ -6,10 +6,10 @@ from build_first_names import create_first_names
 from build_last_names import create_last_names
 
 # source for first names: https://www.ssa.gov/oact/babynames/limits.html 
-ZIP_FIRST_NAMES_PATH = os.path.abspath("names.zip")
+ZIP_FIRST_NAMES_PATH = os.path.abspath("raw/names.zip")
 
 # source for last names: https://www.census.gov/topics/population/genealogy/data/1990_census/1990_census_namefiles.html
-TXT_LAST_NAMES_PATH = os.path.abspath("last_names.txt")
+TXT_LAST_NAMES_PATH = os.path.abspath("raw/last_names.txt")
 
 if __name__ == "__main__":
     setup_logger()
